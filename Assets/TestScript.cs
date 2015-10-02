a08c6163dca05a2a992bd2c2e7b857f6c5bfac9f
Hector Pineiro <hfp1355@rit.edu>
2015-10-02 13:45:21 -0400

    Initial commit with Unity project

---
+++ Assets/TestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    // Can you read this?
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
