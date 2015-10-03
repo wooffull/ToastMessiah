64e0f0bf49294d1b056e88296f16996c7a1167ca
Ben Samuels <bsamuels3791@yahoo.com>
2015-10-03 11:20:00 -0400

    *Implemented folder structure in Unity project. *Added basic character auto-movement

---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour {
+    public float baseSpeed;
+
+    private Transform playerTransform;
+
+	// Use this for initialization
+	void Start () {
+        baseSpeed = 1.0f;
+        playerTransform = gameObject.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //playerTransform.position.x += baseSpeed * Time.deltaTime;
+        playerTransform.Translate(baseSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+	}
+}
