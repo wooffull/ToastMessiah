3650c8f94e703e3997853ebafee6abafbce04df3
Ben Samuels <bsamuels3791@yahoo.com>
2015-10-24 10:28:00 -0400

    *Added different message for winning instead of losing. *Added text about restarting level on Game Over screen

---
+++ Assets/Scripts/WinMarker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class WinMarker : MonoBehaviour
+{
+    private StateManager stateManager;
+
+    void Start(){
+        stateManager = GameObject.Find("GameController").GetComponent<StateManager>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // When running into player, run code!
+        if (other.tag == "Player")
+        {
+            // Change end text to display win message
+            stateManager.endText.GetComponent<Text>().text = "You Win!";
+            stateManager.endText.GetComponent<Text>().color = Color.green;
+            stateManager.SetGameState(StateManager.GameplayState.END);
+        }
+    }
+}
